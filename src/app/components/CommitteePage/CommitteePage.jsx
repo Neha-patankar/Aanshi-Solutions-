61a67f1f7dd322465a9e1173a2a22fac9be15ba0
neha-patankar <nehapatankar122@gmail.com>
2025-03-03 21:06:45 +0530

    fixed some miner issue

--- src/app/components/CommitteePage/CommitteePage.jsx
+++ src/app/components/CommitteePage/CommitteePage.jsx
@@ -38,7 +38,7 @@ const CommitteePage = () => {
         <h1 className="text-center text-2xl font-bold mb-6">
           Committee Members
         </h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-6 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-6">
           {committeeMembers.map((members, index) => (
             <SpeakerCard key={index} members={members} />
           ))}
